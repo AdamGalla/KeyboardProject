f33a855c23b3bb4fd76f2d4b34fcee4742ad7aba
AdamGalla <ado.galla@gmail.com>
2023-05-25 12:20:06 +0200

    Added in memory database

---
+++ KeyboardAPI/Data/KeyboardApiContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using KeyboardAPI.Models;
+
+namespace KeyboardAPI.Data;
+
+public class KeyboardApiContext : DbContext
+{
+    public KeyboardApiContext(DbContextOptions<KeyboardApiContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Keyboard> Keyboards { get; set; }
+}
---
+++ KeyboardAPI/Data/KeyboardRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using KeyboardAPI.Models;
+
+namespace KeyboardAPI.Data;
+
+public class CustomerRepository : IRepository<Customer>
+{
+
+    private readonly CustomerApiContext db;
+
+    public CustomerRepository(CustomerApiContext context)
+    {
+        db = context;
+    }
+
+    Customer IRepository<Customer>.Add(Customer entity)
+    {
+        var newCustomer = db.Customers.Add(entity).Entity;
+        db.SaveChanges();
+        return newCustomer;
+    }
+
+    void IRepository<Customer>.Edit(Customer entity)
+    {
+        db.Entry(entity).State = EntityState.Modified;
+        db.SaveChanges();
+    }
+
+    Customer IRepository<Customer>.Get(int id)
+    {
+        return db.Customers.FirstOrDefault(p => p.Id == id)!;
+    }
+
+    IEnumerable<Customer> IRepository<Customer>.GetAll()
+    {
+        return db.Customers.ToList();
+    }
+
+    void IRepository<Customer>.Remove(int id)
+    {
+        var customer = db.Customers.FirstOrDefault(p => p.Id == id);
+        if (customer != null)
+        {
+            db.Customers.Remove(customer);
+            db.SaveChanges();
+        }
+    }
+}
