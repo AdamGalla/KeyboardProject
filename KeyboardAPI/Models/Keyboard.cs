9fec422235c822a3642453be066d332e3d6e4b59
AdamGalla <ado.galla@gmail.com>
2023-05-25 12:19:41 +0200

    Added models

---
+++ KeyboardAPI/Models/Keyboard.cs
@@ -0,0 +1,9 @@
+﻿namespace KeyboardAPI.Models
+{
+    public class Keyboard
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
