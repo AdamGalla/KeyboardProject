e9d1ba5763777fbede7eb609fbda8f21ea1fd54d
AdamGalla <ado.galla@gmail.com>
2023-05-26 10:47:40 +0200

    Register services

--- KeyboardAPI/Program.cs
+++ KeyboardAPI/Program.cs
@@ -1,7 +1,15 @@
+using KeyboardAPI.Data;
+using KeyboardAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<KeyboardApiContext>(opt => opt.UseInMemoryDatabase("KeyboardsSd"));
+// Register repositories for dependency injection
+builder.Services.AddScoped<IRepository<Keyboard>, KeyboardRepository>();
+// Register database initializer for dependency injection
+builder.Services.AddTransient<IDbInitializer, DbInitializer>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -9,11 +17,28 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+//Configure cors
+app.UseCors(options =>
+{
+    options.AllowAnyOrigin();
+    options.AllowAnyMethod();
+    options.AllowAnyHeader();
+});
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+// Initialize the database.
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var dbContext = services.GetService<KeyboardApiContext>();
+    var dbInitializer = services.GetService<IDbInitializer>();
+    dbInitializer.Initialize(dbContext);
 }
 
 app.UseHttpsRedirection();
