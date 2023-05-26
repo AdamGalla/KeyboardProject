6d4ad825a73feaabfb674fd40f912c946da71579
AdamGalla <ado.galla@gmail.com>
2023-05-26 10:33:26 +0200

    Changes to the keyboard model

--- KeyboardAPI/Data/DbInitializer.cs
+++ KeyboardAPI/Data/DbInitializer.cs
@@ -18,7 +18,8 @@ public class DbInitializer : IDbInitializer
 
         List<Keyboard> keyboards = new List<Keyboard>
         {
-            new Keyboard { Id = 1, Name = "Adam", Url = "" }
+            new Keyboard { Id = 1, Name = "The Swiss Cheeseboard", Image = "", ReservedBy = null },
+            new Keyboard { Id = 2, Name = "Command65", Image = "", ReservedBy = null }
         };
 
         context.Keyboards.AddRange(keyboards);
