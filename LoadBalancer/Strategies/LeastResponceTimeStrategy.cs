f9359ecbf3ac50ae4b3c999e6dc08a7a99fe4068
AlexStavrev <40688355+AlexStavrev@users.noreply.github.com>
2023-05-30 11:25:50 +0200

    Least response time strategy

---
+++ LoadBalancer/Strategies/LeastResponceTimeStrategy.cs
@@ -0,0 +1,9 @@
+﻿namespace LoadBalancer.Strategies;
+
+public class LeastResponceTimeStrategy : IStrategy
+{
+    public Service? GetNextService(IEnumerable<Service> services)
+    {
+        return services.MinBy(service => service.AverageResponseTime) ?? services.First();
+    }
+}
