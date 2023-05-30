a0dac82beb367826c684ab351cfb47b34b693eb9
AlexStavrev <40688355+AlexStavrev@users.noreply.github.com>
2023-05-30 10:51:06 +0200

    LoadBalancer interface

---
+++ LoadBalancer/LoadBalancer/ILoadBalancer.cs
@@ -0,0 +1,13 @@
+﻿using LoadBalancer.Strategies;
+
+namespace LoadBalancer;
+
+public interface ILoadBalancer
+{
+    public IEnumerable<Service> GetAllServices();
+    public Guid AddService(Service service);
+    public bool RemoveService(Service service);
+    public IStrategy GetActiveStrategy();
+    public bool SetActiveStrategy(IStrategy activeStrategy);
+    public Service? GetNextService();
+}
