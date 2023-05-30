d6935bb983e446af8610337eb7a19c426804f476
AlexStavrev <40688355+AlexStavrev@users.noreply.github.com>
2023-05-30 10:50:54 +0200

    Service model class

---
+++ LoadBalancer/LoadBalancer/Service.cs
@@ -0,0 +1,13 @@
+﻿namespace LoadBalancer;
+
+public record Service
+{
+    public Guid Id { get; set; }
+    public string Url { get; set; }
+
+    public Service(string url)
+    {
+        Id = new Guid();
+        Url = url;
+    }
+}
