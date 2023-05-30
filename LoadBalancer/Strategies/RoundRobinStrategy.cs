8428a0f7c3f15edc4676d9af322e13cabb7f4af9
AlexStavrev <40688355+AlexStavrev@users.noreply.github.com>
2023-05-30 11:15:04 +0200

    Create RoundRobinStrategy.cs

---
+++ LoadBalancer/Strategies/RoundRobinStrategy.cs
@@ -0,0 +1,25 @@
+﻿namespace LoadBalancer.Strategies;
+
+public class RoundRobinStrategy : IStrategy
+{
+    private int _index = 0;
+
+    public Service? GetNextService(IEnumerable<Service> services)
+    {
+        lock(this)
+        {
+            if(!services.Any())
+            {
+                return null;
+            }
+            if(_index >= services.Count())
+            {
+                _index = 0;
+            }
+
+            var service = services.ElementAtOrDefault(_index);
+            _index++;
+            return service;
+        }
+    }
+}
